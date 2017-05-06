73d26664f3967ffd78c1d19ea90169a36cb7b034
Dimitris Grammatikogiannis <d.grammatiko@gmail.com>
2017-05-06 14:37:56 +0300

    error page

--- installation/template/js/error-locale.js
+++ installation/template/js/error-locale.js
@@ -1,14 +1,16 @@
 window.errorLocale = {
-	"en-UK": {
-		"language": "English UK",
+	"en-GB": {
+		"language": "English GB",
 		"header": "Sorry, your PHP version is not supported",
-		"text1": "Your host needs to use PHP version",
-		"text2": "or newer to run this version of Joomla!"
+		"text1": "Your host needs to use PHP version {{phpversion}} or newer to run this version of Joomla!",
+		"help-url": "https://docs.joomla.org/en/unsupported-php-version",
+		"help-url-text": "Help me resolve this"
 	},
 	"el-GR": {
 	    "language": "Ελληνικά",
 		"header": "'Ενα ασχετο μήνυμα ",
-		"text1": "Ο διακομιστής πρέπει να έχει εγκατεστημένη PHP έκδοση",
-		"text2": "ή νεότερη για να εγκαταστήσετε την συγκεκριμένη έκδοση το Joomla!"
+		"text1": "Ο διακομιστής πρέπει να έχει εγκατεστημένη PHP έκδοση {{phpversion}} ή νεότερη για να εγκαταστήσετε την συγκεκριμένη έκδοση το Joomla!",
+		"help-url": "https://docs.joomla.org/gr/unsupported-php-version",
+		"help-url-text": "Κάτι με βοήθεια κτλ"
 	},
 };
--- installation/template/js/error.js
+++ installation/template/js/error.js
@@ -1,8 +1,11 @@
 (function() {
+	"use strict";
 	if (errorLocale) {
 		var header = document.getElementById('headerText'),
 			text1 = document.getElementById('descText1');
-			text2 = document.getElementById('descText2');
+
+		// Get the minimum PHP version
+		window.phpVersion = document.body.getAttribute('data-php-version');
 
 		// Create links for all the languages
 		Object.keys(errorLocale).forEach(function(key) {
@@ -20,8 +23,13 @@
 				var ref = e.target.getAttribute('data-code');
 				if (ref) {
 					header.innerHTML = errorLocale[ref].header;
-					text1.innerHTML = errorLocale[ref].text1;
-					text2.innerHTML = errorLocale[ref].text2;
+					text1.innerHTML = errorLocale[ref].text1.replace('{{phpversion}}', phpVersion);
+				}
+
+				var helpLink = document.getElementById('linkHelp');
+				if (helpLink) {
+					helpLink.href = errorLocale[ref]["help-url"];
+					helpLink.innerText = errorLocale[ref]["help-url-text"];
 				}
 			});
 			li.appendChild(aLink);
