e143c1a29845d340b94fb95f265c71a15468868b
dgt41 <d.grammatiko@gmail.com>
2017-05-30 17:24:48 +0300

    preinstall

--- installation/view/preinstall/tmpl/default.php
+++ installation/view/preinstall/tmpl/default.php
@@ -26,7 +26,7 @@ defined('_JEXEC') or die;
 </form>
 <form action="index.php" method="post" id="adminForm">
 	<div class="row">
-		<div class="col-md-6">
+		<div class="col-md-12">
 			<h3><?php echo JText::_('INSTL_PRECHECK_TITLE'); ?></h3>
 			<hr>
 			<p class="install-text">
@@ -35,6 +35,7 @@ defined('_JEXEC') or die;
 			<table class="table table-striped table-sm">
 				<tbody>
 					<?php foreach ($this->options as $option) : ?>
+					<?php if ($option->state === 'JNO' || $option->state === false) : ?>
 					<tr>
 						<td class="item">
 							<?php echo $option->label; ?>
@@ -48,6 +49,55 @@ defined('_JEXEC') or die;
 							</span>
 						</td>
 					</tr>
+						<?php if ($option->state === false && preg_match('$configuration.php$', $option->label)) : ?>
+						<tr>
+							<form action="index.php" method="post" id="adminForm" class="form-validate">
+								<h3><?php echo JText::_('INSTL_FTP'); ?></h3>
+								<hr>
+								<div class="form-group">
+									<?php echo $this->form->getLabel('ftp_enable'); ?>
+									<?php echo $this->form->getInput('ftp_enable'); ?>
+								</div>
+								<div class="form-group">
+									<?php echo $this->form->getLabel('ftp_user'); ?>
+									<?php echo $this->form->getInput('ftp_user'); ?>
+									<p class="form-text text-muted small"><?php echo JText::_('INSTL_FTP_USER_DESC'); ?></p>
+								</div>
+								<div class="form-group">
+									<?php echo $this->form->getLabel('ftp_pass'); ?>
+									<?php echo $this->form->getInput('ftp_pass'); ?>
+									<p class="form-text text-muted small"><?php echo JText::_('INSTL_FTP_PASSWORD_DESC'); ?></p>
+								</div>
+								<div class="form-group">
+									<button id="verifybutton" class="btn btn-success" onclick="Install.verifyFtpSettings(this);"><span class="icon-ok icon-white"></span> <?php echo JText::_('INSTL_VERIFY_FTP_SETTINGS'); ?></button>
+								</div>
+								<div class="form-group">
+									<?php echo $this->form->getLabel('ftp_host'); ?>
+									<div class="input-append">
+										<?php echo $this->form->getInput('ftp_host'); ?><button id="findbutton" class="btn btn-secondary" onclick="Install.detectFtpRoot(this);"><span class="icon-folder-open"></span> <?php echo JText::_('INSTL_AUTOFIND_FTP_PATH'); ?></button>
+									</div>
+								</div>
+								<div class="form-group">
+									<?php echo $this->form->getLabel('ftp_port'); ?>
+									<?php echo $this->form->getInput('ftp_port'); ?>
+								</div>
+								<div class="form-group">
+									<?php echo $this->form->getLabel('ftp_save'); ?>
+									<?php echo $this->form->getInput('ftp_save'); ?>
+								</div>
+								<div class="btn-toolbar justify-content-end">
+									<div class="btn-group">
+										<a class="btn btn-secondary" href="#" onclick="return Install.goToPage('database');" rel="prev" title="<?php echo JText::_('JPREVIOUS'); ?>"><span class="fa fa-arrow-left"></span> <?php echo JText::_('JPREVIOUS'); ?></a>
+										<a class="btn btn-primary" href="#" onclick="Install.submitform();" rel="next" title="<?php echo JText::_('JNEXT'); ?>"><span class="fa fa-arrow-right icon-white"></span> <?php echo JText::_('JNEXT'); ?></a>
+									</div>
+								</div>
+								<input type="hidden" name="task" value="ftp">
+								<input type="hidden" name="format" value="json">
+								<?php echo JHtml::_('form.token'); ?>
+							</form>
+						</tr>
+						<?php endif; ?>
+					<?php endif; ?>
 					<?php endforeach; ?>
 				</tbody>
 				<tfoot>
@@ -57,7 +107,7 @@ defined('_JEXEC') or die;
 				</tfoot>
 			</table>
 		</div>
-		<div class="col-md-6">
+		<div class="col-md-12">
 			<h3><?php echo JText::_('INSTL_PRECHECK_RECOMMENDED_SETTINGS_TITLE'); ?></h3>
 			<hr>
 			<p class="install-text"><?php echo JText::_('INSTL_PRECHECK_RECOMMENDED_SETTINGS_DESC'); ?></p>
